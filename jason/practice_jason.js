45798265a0be93e41905fdfe55d57f9b229ccf12
eustaquio <eustaquio.galano@gmail.com>
2025-03-15 15:18:44 +0800

    feat: add generate function
    This function does the following:
    - fetch the json file for rendering purposes
    - invoke the function that will render the generated contents

--- jason/practice_jason.js
+++ jason/practice_jason.js
@@ -1,1 +1,13 @@
-console.log("here");
+async function generate() {
+    const requestURL = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const superHeroes = await response.json();
+    console.log(superHeroes);
+
+    generateHeader(superHeroes);
+    // generateMain(superHeroes);
+}
+
+generate();
