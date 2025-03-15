f147943f2bc1e96a0f8c6bf6f75db54e787ac059
eustaquio <eustaquio.galano@gmail.com>
2025-03-15 15:25:04 +0800

    feat: add generate header function
    The function does the following:
    - Get a reference to the header
    - Create h1 element that will hold the sqad name
    - Create a p element that will hold the home town and year formed
    - Append this 2 elements inside header

--- jason/practice_jason.js
+++ jason/practice_jason.js
@@ -10,4 +10,18 @@ async function generate() {
     // generateMain(superHeroes);
 }
 
+function generateHeader(obj) {
+    const header = document.querySelector('header');
+    
+    const squadName = document.createElement('h1');
+    squadName.textContent = obj.squadName;
+
+    header.appendChild(squadName);
+
+    const quickInfo = document.createElement('p');
+    quickInfo.textContent = `Home: ${obj.homeTown} Formed: ${obj.formed}`;
+
+    header.appendChild(quickInfo);
+}
+
 generate();
