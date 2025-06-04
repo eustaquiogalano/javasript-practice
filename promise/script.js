fa1229afff59e704ae0d2d4ef855c6ed53d6cfad
eustaquio <eustaquio.galano@gmail.com>
2025-06-04 13:31:30 +0800

    add finally function for practice

--- promise/script.js
+++ promise/script.js
@@ -7,7 +7,7 @@
 function walkDog() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const dogWalked = true;
+      const dogWalked = false;
       if (dogWalked === "vague") {
         resolve("Walk him now!");
       } else if (dogWalked) {
@@ -29,4 +29,7 @@ walkDog()
   })
   .catch((message) => {
     console.error(Error(message));
+  })
+  .finally(() => {
+    console.log("Done!");
   });
