c37baa01800d22898bb44c040d96b9cbb3c5f4fd
eustaquio <eustaquio.galano@gmail.com>
2025-06-04 14:04:35 +0800

    additional practice for promise

--- promise/script.js
+++ promise/script.js
@@ -7,11 +7,9 @@
 function walkDog() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const dogWalked = false;
-      if (dogWalked === "vague") {
-        resolve("Walk him now!");
-      } else if (dogWalked) {
-        resolve("Dog already happy!");
+      const dogWalked = true;
+      if (dogWalked) {
+        resolve("Dog happy!");
       } else {
         reject("Dog sad :(");
       }
@@ -19,17 +17,34 @@ function walkDog() {
   });
 }
 
+// additional function to practice chaining in promise
+function feedDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("The Dog is full now and happy");
+    }, 5000);
+  });
+}
+
+// chaining .then, so that you dont need the callback hell
 walkDog()
   .then((message) => {
+    // logging the message if promise is resolved
     console.log(message);
-    return message;
+    // then invoking the next chore for the dog
+    return feedDog();
   })
   .then((message) => {
+    // then a little reminder for the owner
+    // and log the resolve message from feedDog function
     console.log("walk the dog everyday " + message);
   })
   .catch((message) => {
+    // throw the error
     console.error(Error(message));
   })
   .finally(() => {
+    // a reminder message that the promise is done
+    // success of fail
     console.log("Done!");
   });
